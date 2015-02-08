e62e8b4975bdf32b5f8828c264a066b5ca519463
Alex <rogachev-alex@yandex.ru>
2015-02-08 19:10:29 +0200

    Changed creds to vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ EmployeeManager::Application.configure do
   config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-          :bucket => ENV['swansoftwaresolutions'],
-          :access_key_id => ENV['AKIAIJBRQEXCLMT65TOA'],
-          :secret_access_key => ENV['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']
+          :bucket => ENV['S3_BUCKET_NAME'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
   }
 
