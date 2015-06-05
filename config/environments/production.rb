e4322a361169f375d540d700def660cebdef79de
alex-rogachev <rogachev-alex@yandex.ru>
2015-06-05 16:46:42 +0300

    Setting up S3 and SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 EmployeeManager::Application.configure do
 
-  Paperclip::Attachment.default_options[:s3_host_name] = 'https://s3.eu-central-1.amazonaws.com'
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3.eu-central-1.amazonaws.com'
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
   # Settings specified here will take precedence over those in config/application.rb
@@ -81,6 +81,7 @@ EmployeeManager::Application.configure do
 
   config.paperclip_defaults = {
       :storage => :s3,
+      :s3_protocol => :https,
       :s3_credentials => {
           :bucket => ENV['S3_BUCKET_NAME'],
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
