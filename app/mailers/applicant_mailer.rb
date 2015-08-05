4e8a6fed977d3fc9da4df2bc0426e5407dc12f4a
alex-rogachev <rogachev-alex@yandex.ru>
2015-08-05 17:25:15 +0300

    Changed email content

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -2,9 +2,9 @@ class ApplicantMailer < ActionMailer::Base
   def welcome_message applicant
     @applicant = applicant
 
-    mail subject: 'Welcome message',
-         from: 'Swan Software Solutions <no-reply@swansoftwaresolutions.com>',
+    mail subject: 'Glass Software Solutions',
+         from: 'Natalia Ignatieva <recruiter.ignatieva@gmail.com>',
          to: 'rogachev-alex@yandex.ru',
-         reply_to: ''
+         reply_to: 'recruiter.ignatieva@gmail.com'
   end
 end
