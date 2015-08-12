6e963a49ae0d326f11c84d7e96dba1278d4ffa12
alex-rogachev <rogachev-alex@yandex.ru>
2015-08-12 14:00:29 +0300

    Improved employee sending histories table

--- app/admin/email_sending_history.rb
+++ app/admin/email_sending_history.rb
@@ -1,13 +1,16 @@
 ActiveAdmin.register EmailSendingHistory do
+  actions :index
+  batch_action :destroy, false
+  config.filters = false
+
   breadcrumb {}
 
   index do
-    selectable_column
-    column(:recipient, sortable: :recipient) { |historical_record| historical_record.emailable.decorate.full_name }
-    column(:recipient_type, sortable: :recipient_type) { |historical_record| historical_record.emailable_type }
-    column(:recipient_email_address, sortable: :recipient_email_address) { |historical_record| historical_record.emailable.email }
-    column(:letter_type, sortable: :letter_type) { |historical_record| format_value historical_record, :name, type: 'Email' }
-    column(:sent_by, sortable: :sent_by) { |historical_record| AdminUser.find(historical_record.sent_by).decorate.full_name }
-    column(:sent_at, sortable: :sent_at) { |historical_record| historical_record.created_at }
+    column(:recipient) { |historical_record| historical_record.emailable.decorate.full_name }
+    column(:recipient_type) { |historical_record| historical_record.emailable_type }
+    column(:recipient_email_address) { |historical_record| historical_record.emailable.email }
+    column(:letter_type) { |historical_record| format_value historical_record, :name, type: 'Email' }
+    column(:sent_by) { |historical_record| AdminUser.find(historical_record.sent_by).decorate.full_name }
+    column(:sent_at) { |historical_record| historical_record.created_at.to_formatted_s(:db) }
   end
 end
