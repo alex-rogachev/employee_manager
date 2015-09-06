5aee43bf40b3349f70438eee209bb126afddd595
alex-rogachev <rogachev-alex@yandex.ru>
2015-09-06 21:20:03 +0300

    Implemented show action for email templates

--- app/admin/email_template.rb
+++ app/admin/email_template.rb
@@ -4,7 +4,12 @@ ActiveAdmin.register EmailTemplate do
   config.filters = false
 
   controller do
-    before_filter :set_email_template, only: [:edit, :update]
+    before_filter :set_email_template, only: [:edit, :update, :show]
+
+    def show
+      @partial = "/applicant_mailer/#{@email_template.name}"
+      render layout: 'active_admin'
+    end
 
     def edit
       @email_template_form = EM::EmailTemplate::Invitation::Form.new(
@@ -34,20 +39,6 @@ ActiveAdmin.register EmailTemplate do
     column(:name) { |email_template| email_template.name.capitalize }
     actions defaults: false do |email_template|
       link_to 'Show', admin_email_template_path(email_template), class: 'table_links'
-    end
-  end
-
-  show do |email_template|
-    attributes_table do
-      row(:name) { |email_template| email_template.name.capitalize }
-      row :data do
-        data = ''
-        email_template.data.each_pair do |attr, value|
-          # TODO: rework string generation
-          data << "#{attr.to_s.capitalize.gsub('_', ' ')}: #{value} "
-        end
-        data
-      end
     end
   end
 end
