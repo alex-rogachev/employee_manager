2f42a484067a528fcf67f8cec3a068f092f7b9a0
alex-rogachev <rogachev-alex@yandex.ru>
2015-09-23 19:07:14 +0300

    Fixed mistake

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,7 +2,7 @@ class WebhooksController < ApplicationController
   def sendgrid_event
     events = params[:_json]
     events.each do |event|
-      emailable = Applicant.find(event[:email])
+      emailable = Applicant.find_by_email(event[:email])
       log_record = EmailSendingLog.find_or_initialize_by_sg_id(event[:sg_message_id])
       log_record.assign_attributes(status: event[:event], emailable_id: emailable.id, emailable_type: emailable.class.name)
       log_record.save!
