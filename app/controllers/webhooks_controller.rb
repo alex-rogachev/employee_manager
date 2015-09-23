2ab86f936f2d402eec6f5461ef4608ed796ec765
alex-rogachev <rogachev-alex@yandex.ru>
2015-09-23 22:48:33 +0300

    Commented out user id

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -4,7 +4,7 @@ class WebhooksController < ApplicationController
     events.each do |event|
       emailable = Applicant.find_by_email(event[:email])
       log_record = EmailSendingLog.find_or_initialize_by_sg_id(event[:sg_message_id])
-      log_record.assign_attributes(status: event[:event], emailable_id: emailable.id, emailable_type: emailable.class.name, sent_by: current_admin_user.id)
+      log_record.assign_attributes(status: event[:event], emailable_id: emailable.id, emailable_type: emailable.class.name)
       log_record.save!
     end
 
--- app/services/applicants/send_invitation.rb
+++ app/services/applicants/send_invitation.rb
@@ -6,7 +6,6 @@ module Applicants
       ids = context.ids
 
       raise ArgumentError, 'IDs must be specified' if ids.empty?
-      raise ArgumentError, 'User id must be specified' if user_id.nil?
 
       applicants = Applicant.where(id: ids)
       applicants_with_emails = applicants.where('email IS NOT NULL AND email != ?', '')
